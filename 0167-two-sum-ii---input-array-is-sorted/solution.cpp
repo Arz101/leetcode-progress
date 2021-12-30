62623f2e970a37bfabeb13ba027f7e44076afcfa
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2021-12-30 22:29:03 +0000

    Sync LeetCode submission Runtime - 8 ms (2.08%), Memory - 9.8 MB (100.00%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.cpp
+++ 0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> result;
-        int i = 0;
-        int j = numbers.size()-1;
+        vector<int>ans;
+        map<int,int>m1;
+        int n=numbers.size(),num;
         
-        while(i < numbers.size() && j < numbers.size() && i < j){
-            if(numbers[i] + numbers[j] == target){
-                result.push_back(i+1);
-                result.push_back(j+1);
-                return result;
+        for(int i=0;i<n;++i){
+            num = target - numbers[i];
+            
+            if(m1.find(num) != m1.end()){
+                ans.push_back(m1[num] + 1);
+                ans.push_back(i + 1);
+                return ans;
             }
-            else if(numbers[i] + numbers[j] < target){
-                i++;
-            }
-            else{
-                j--;
-            }
+            m1[numbers[i]] = i;
         }
-        
-        return result;
+        return {};
     }
 };
