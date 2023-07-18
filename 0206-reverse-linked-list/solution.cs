40cc0262bbcee23fed8d18e85791b25eb7bb32aa
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2023-07-18 16:49:05 +0000

    Sync LeetCode submission Runtime - 90 ms (1.41%), Memory - 38.7 MB (100.00%)

---
+++ 0206-reverse-linked-list/solution.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode temp;
+        
+        while(head != null){
+            temp = head.next;
+
+            head.next = prev;
+
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
