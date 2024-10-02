170802880001e029ecb81505b009d6f3290706c8
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2024-10-02 16:33:47 +0000

    Sync LeetCode submission Runtime - 2 ms (99.34%), Memory - 45 MB (99.97%)

--- 0001-two-sum/solution.java
+++ 0001-two-sum/solution.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        
-        int cop;
+
         for(int i = 0; i < nums.length; i++){
-            cop = target - nums[i];
-            if(map.containsKey(cop)){
-                return new int[] {map.get(cop),i};
-            }
+            int comp = target - nums[i];
+            if(map.containsKey(comp)) return new int[] {i, map.get(comp)};
             else map.put(nums[i], i);
-        }   
+        }
         return new int[]{};
     }
 }
