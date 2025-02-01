79b504b898d6c2ba42f29f3fbed283c9c0637025
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2025-02-01 18:37:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.6 MB (61.48%)

---
+++ 3429-special-array-i/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        
+        if(nums.size() == 1) return true;
+
+        for(size_t i = 0; i < nums.size(); ++i){
+            if(i >= nums.size()-1) break;
+            if(nums[i] % 2 == 0 && nums[i+1] % 2 != 0){
+                continue;
+            }
+            else if(nums[i] % 2 != 0 && nums[i+1] % 2 == 0){
+                continue;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
