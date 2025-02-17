256f635205a9b2492677656ccd76146c9ffefb8d
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2025-02-17 19:57:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.3 MB (15.57%)

--- 2210-find-target-indices-after-sorting-array/solution.cpp
+++ 2210-find-target-indices-after-sorting-array/solution.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        vector<int> ans;
         sort(nums.begin(),nums.end());
-        
-        for(int i = 0; i < nums.size(); ++i){
-            if(nums[i] == target) ans.push_back(i); 
+        vector<int> ans;
+        for(int it = 0; it < nums.size(); it++){
+            if(nums[it] == target) ans.push_back(it);
+            else {
+                if(nums[it] > target)break;
+            }
         }
         return ans;
     }
