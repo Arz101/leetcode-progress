db2a34d4faca65b4c5d886fd830185654bb6f643
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2025-02-18 14:55:41 +0000

    Sync LeetCode submission Runtime - 5 ms (36.03%), Memory - 38.9 MB (21.90%)

---
+++ 0653-two-sum-iv---input-is-a-bst/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> set;
+        return find(root, k, set);
+    }
+    bool find(TreeNode* root, int k, unordered_set<int>& set){
+        if(!root) return false;
+
+        int comp = k - root->val;
+        if(set.find(comp) != set.end()){
+            return true;
+        }
+        else set.insert(root->val);
+
+        return find(root->left, k, set) || find(root->right, k, set);
+    }
+};
