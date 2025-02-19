4dd8f72f11fbe42a7de5af32ebf6164db15b8430
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2025-02-19 19:46:29 +0000

    Sync LeetCode submission Runtime - 62 ms (20.18%), Memory - 77.2 MB (75.78%)

--- 0217-contains-duplicate/solution.cpp
+++ 0217-contains-duplicate/solution.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        for (int num : nums) {
-            if (seen.count(num) > 0)
-                return true;
-            seen.insert(num);
-        }
-        return false;
+        return !(nums.size()==set(nums.begin(),nums.end()).size());
     }
 };
