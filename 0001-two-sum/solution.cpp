125b3ab65b8375762b258172e8b83cf390e01aa8
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2025-05-03 04:25:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.8 MB (54.23%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> map;
+        unordered_map<int,int> map;
 
-        for(int i=0; i < nums.size(); i++){
+
+        for(int i = 0; i < nums.size(); i++){
             int comp = target - nums[i];
+
             if(map.find(comp) != map.end()){
-                return std::vector<int>{map[comp], i};
+                return {i, map[comp]};
             }
-            else map.insert(std::make_pair(nums[i], i));
+            else map[nums[i]] = i;
         }
-        return std::vector<int>{};
+        return {};
     }
-};
+}; // O (n^2)  -  O(n)
