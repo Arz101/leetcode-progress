b323d37a7676727a508cdf42ac698db39db61b2c
Adrian Rodriguez <81156314+Arz101@users.noreply.github.com>
2025-05-29 14:55:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.5 MB (100.00%)

--- 0035-search-insert-position/solution.py
+++ 0035-search-insert-position/solution.py
@@ -1,22 +1,23 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        
-        MIN = 0
-        MAX = len(nums)
-        
-        if nums[-1] < target:
-          return len(nums)
-        
-        while MIN <= MAX:
-          i = int((MIN+MAX)/2)
-        
-          if nums[i] == target:
-            return i  
-          
-          if nums[i] > target:
-            MAX = i - 1
 
-          if nums[i] < target:
-            MIN = i + 1
-          
-        return MIN
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        min = 0
+        max = len(nums) - 1
+
+        while min <= max:
+            mid = min + (max - min) // 2
+
+            if nums[mid] == target: 
+                return mid
+
+            elif nums[mid] < target:
+                min = mid + 1
+
+            else:
+                max = mid - 1
+        return mid if nums[mid] > target else mid +1 
+
+
+
+
+
